4198f7a142d78b58f0554b45adb32741e43ef014
TarunVanshkar <117818351+TarunVanshkar@users.noreply.github.com>
2023-04-16 17:59:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const element = document.getElementById("colorSelect");
+const button = document.getElementById("button");
+
+
+function removeColor(){
+    let colorRemoved=element.value;
+    element.remove(colorRemoved);
+}
